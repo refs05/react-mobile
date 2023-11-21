2a82dd3543a4613f573028052462d8009d702095
Restu  Fajar Sidhiq <64077625+refs05@users.noreply.github.com>
2023-11-21 18:26:09 +0700

    apply react navigation

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './pages/home/home';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Overview'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text>this is home</Text>
+    </View>
+  );
+}
+
+export default Home;
